9716c74618c7b504786d2d3dc6f3468159a32247
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-12 16:05:17 +0200

    Add modal comp

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref='name'>
-  <button @click='handleClick'>Click me</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from "./components/Modal.vue";
 export default {
   name: "App",
+  components: {Modal},
   data() {
     return {
       title: "My first Vue App",
     };
   },
-  methods:{
-    handleClick(){
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-    }
-  }
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+    },
+  },
 };
 </script>
 
@@ -35,7 +36,5 @@ h1 {
   display: inline-block;
   padding-bottom: 10px;
 }
-.active {
-  background: red;
-}
+
 </style>
