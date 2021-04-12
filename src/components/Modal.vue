0a9c068163483759500e209fb65431b97aa5b413
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-12 16:14:11 +0200

    Scoped & global css

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="backdrop">
     <div class="modal">
+      <h1>Modal title</h1>
       <p>modal content</p>
     </div>
   </div>
 </template>
 
+<!-- <style scoped> -->
 <style>
 .modal {
   width: 400px;
@@ -20,5 +22,13 @@
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+.modal h1 {
+  color: #03cfb4;
+  padding: 0;
+  border: none;
+}
+.modal p {
+  font-style: normal;
 }
 </style>
